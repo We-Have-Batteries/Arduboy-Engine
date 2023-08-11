84f9cb30cc8cbec46469dd88b639966990b6e82d
SethByrd612 <73444767+SethByrd612@users.noreply.github.com>
2023-08-11 03:06:20 -0400

    Added the map editor which is currently broken
    or some reason, pushback isn't doing anything...

---
+++ MapEditor.cpp
@@ -0,0 +1,46 @@
+#include "MapEditor.h"
+#include "AbstractObject.h"
+#include "GameObject.h"
+#include "Setup.h"
+#include "GameWorld.h"
+
+const MapEditor::MethodType MapEditor::prefabMethods[4] = { Prefabs::Air, Prefabs::Brick, Prefabs::Player, Prefabs::Img };
+
+const uint8_t MapEditor::map1[6] = { 
+  2, 2, 
+  2, 1,
+  2, 1,
+};
+
+void MapEditor::CreateMap(uint8_t* map, float xOffset, float yOffset){
+  // for the width times the height
+  float xPos = xOffset;
+  float yPos = yOffset;
+
+  // Setup::tinyfont.print("CreatingMap: ");
+
+  // Ignore the first two numbers, as that's our width and height of the map
+  for (int i = 2; i < map[0] * map[1] + 2; i++){
+    // if we hit the width, go down by one on the Y and reset the x back to 0
+    if (i % map[0] == 0){
+      yPos += gameWorldHeightPadding;
+      xPos = xOffset;
+    }
+    AbstractObject obj = MapEditor::prefabMethods[map[i]]();
+    static_cast<Image*>(obj.objectPtr)->SetX(xPos);
+    static_cast<Image*>(obj.objectPtr)->SetY(yPos);
+    switch(obj.type){
+      case AbstractObject::IMAGE:
+        GameWorld::AddObject(static_cast<Image*>(obj.objectPtr));
+        break;
+      case AbstractObject::COLLIDABLEIMAGE:
+        GameWorld::AddObject(static_cast<CollidableImage*>(obj.objectPtr));
+        break;
+      case AbstractObject::GAMEOBJECT:
+        GameWorld::AddObject(static_cast<GameObject*>(obj.objectPtr));
+        break;
+    }
+    xPos += gameWorldWidthPadding;
+  }
+}
+
--- MapEditor.h
+++ MapEditor.h
@@ -2,16 +2,28 @@
 #define MAPEDITOR_H
 
 #include "flagsapi.h"
+#include "Prefabs.h"
+#include "GameWorld.h"
+
+class AbstractObject;
+class CollidableImage;
 
 class MapEditor{
 public:
-  uint8_t map1[] = {4, 
-  0, 0, 0, 0, 
-  1, 1, 1, 1, 
-  1, 1, 1, 1, 
-  0, 0, 0, 0}
+  using MethodType = AbstractObject(*)();
 
+  static const int gameWorldWidthPadding = 4;
+  static const int gameWorldHeightPadding = 4;
 
+  static const uint8_t map1[6];
+
+  static const MethodType prefabMethods[4];
+
+  static CollidableImage* GetBrick(){
+    return static_cast<CollidableImage*> (prefabMethods[1]().objectPtr);
+  }
+
+  static void CreateMap(uint8_t* map, float xOffset = 0, float yOffset = 0);
 };
 
 #endif
